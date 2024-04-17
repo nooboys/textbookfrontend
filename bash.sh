9418ac2940efcc3a931db35f43c825e8543859cf
vishnu <vishnu1961993@gmail.com>
2024-04-17 09:48:12 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
 mkdir builds
 ls -alt
 pwd
+docker run -v "$PWD:/pwd" ghcr.io/trufflesecurity/trufflehog:latest github --repo https://github.com/trufflesecurity/test_keys --debug  > output.txt
+ls -alh
